30d85b19a1e9bfad1d34319fb08c93ddcf01bd04
ghada258 <161519992+ghada258@users.noreply.github.com>
2025-05-31 15:50:37 +0300

    add yaml

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 sudo apt-get install cowsay -y
-cowsay -f dragon "Run for cover, i am a Dragon..... RAWR" >> dargon.txt
-grep -i "dragon" dragon.txt
+cowsay -f dragon "Run for cover, iam a Dragon.... RAVR" » dragon. txt 
+grep -i "dragon" dragon. txt 
 cat dragon.txt
-ls -ltra
+ls -ltra
